2b7a67eaad32e3d7d957dd15b03646c231f01a6c
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-03 14:25:22 -0400

    provide disclosure context to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { ChakraProvider, theme } from '@chakra-ui/react'
 
+import {DisclosureProvider} from "components/DisclosureProvider"
 import React from "react"
 
 export const parameters = {
@@ -9,7 +10,9 @@ export const parameters = {
 const withChakra = (StoryFn) => {
   return (
     <ChakraProvider theme={theme}>
+      <DisclosureProvider>
       <StoryFn />
+      </DisclosureProvider>
     </ChakraProvider>
   )
 }
