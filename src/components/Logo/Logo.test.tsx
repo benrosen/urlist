93dfde56e0fafe0e826d2656350b91c9f89f3d5d
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-03 19:02:55 -0400

    update config import

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -2,12 +2,12 @@ import { render, screen } from "@testing-library/react";
 
 import Logo from "./index";
 import React from "react";
-import { appTitle } from "./Logo.data.json";
+import config from "./Logo.config.json";
 
 describe("The Logo component", () => {
   it("renders the title of the application.", () => {
     render(<Logo />);
-    const logoElement = screen.getByText(appTitle);
+    const logoElement = screen.getByText(config.appTitle);
     expect(logoElement).toBeInTheDocument();
   });
 });
